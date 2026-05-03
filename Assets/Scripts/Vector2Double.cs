1b499dd67df76a86bcb0270de2382cffe40d796c
icho <v2225384@gmail.com>
2026-05-03 19:41:01 +0300

    Vector2Double fix

--- Assets/Scripts/Vector2Double.cs
+++ Assets/Scripts/Vector2Double.cs
@@ -17,6 +17,7 @@ public struct Vector2Double
 
     public static Vector2Double operator +(Vector2Double a, Vector2Double b) => new(a.x + b.x, a.y + b.y);
     public static Vector2Double operator -(Vector2Double a, Vector2Double b) => new(a.x - b.x, a.y - b.y);
+    public static Vector2Double operator -(Vector2Double a) => new(-a.x, -a.y);
     public static Vector2Double operator *(Vector2Double a, Vector2Double b) => new(a.x * b.x, a.y * b.y);
     public static Vector2Double operator *(Vector2Double a, double b) => new(a.x * b, a.y * b);
     public static Vector2Double operator /(Vector2Double a, double b) => new(a.x / b, a.y / b);
@@ -28,8 +29,15 @@ public struct Vector2Double
     public static implicit operator Vector2(Vector2Double a) => new((float)a.x, (float)a.y);
     public static implicit operator Vector3(Vector2Double a) => new((float)a.x, (float)a.y, 0);
     
-    public double magnitude => Math.Sqrt((x * x) + (y * y));
-    public Vector2Double normalized => new (x / magnitude, y / magnitude);
+    public double magnitude => Math.Sqrt(x * x + y * y);
+    public Vector2Double normalized => magnitude > 0 ? new(x / magnitude, y / magnitude) : zero;
     
-    public static double Distance(Vector2Double a, Vector2Double b) => Math.Sqrt(a.x * a.x + a.y * a.y) ;
+    public static double Distance(Vector2Double a, Vector2Double b)
+    {
+        var dx = a.x - b.x;
+        var dy = a.y - b.y;
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
+
+    public override string ToString() => $"({x:e}, {y:e})";
 }
